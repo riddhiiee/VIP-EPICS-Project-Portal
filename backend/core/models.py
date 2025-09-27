2888cd7aecd15930506459b7449ddc6842946f52
Riddhi Bhagat <140200617+riddhiiee@users.noreply.github.com>
2025-09-27 14:55:54 +0530

    updated models

--- backend/core/models.py
+++ backend/core/models.py
@@ -19,13 +19,19 @@ class Project(models.Model):
         return self.title
 
 class Student(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    fullname = models.CharField(max_length=100)
     sapid = models.CharField(max_length=11, unique=True)
-    name = models.CharField(max_length=100)
-    year = models.IntegerField(max_length=1)
-    semester = models.IntegerField(max_length=2) 
-    degree = models.CharField(max_length=100)   
+    email = models.EmailField(unique=True)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    degree = models.CharField(max_length=100)
     department = models.CharField(max_length=100)
+    division = models.CharField(max_length=1)
+    password = models.CharField(max_length=100)
+    campus = models.CharField(max_length=100)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.name
