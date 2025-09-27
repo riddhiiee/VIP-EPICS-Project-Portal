929fe02657b9090bbbfdeb9b1f0229067777a0b1
Riddhi Bhagat <140200617+riddhiiee@users.noreply.github.com>
2025-09-27 16:59:43 +0530

    added path to POST student

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import post_student
 
 urlpatterns = [
-    path('student/', view=post_student, name="post_student")
+    path('student/', post_student, name="post_student")
 ]
