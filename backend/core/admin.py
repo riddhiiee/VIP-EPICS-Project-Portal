e62229cae7c810751db25a28c584534190dd4d7d
Krishna <krishnaj.joshi24@gmail.com>
2025-09-27 17:35:58 +0530

    changed admin.py and models.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Faculty, Student
+from .models import Faculty, Student, Project, Application
 
 # Register your models here.
 admin.site.register(Faculty)
 admin.site.register(Student)
+admin.site.register(Project)
+
+@admin.register(Application)
+
+class ApplicationAdmin(admin.ModelAdmin):
+    actions = ["accept_application", "reject_application"]
+
+    def accept_application(self, request, queryset):
+        queryset.update(status="Accepted")
+    accept_application.short_description = "Mark selected applications as Accepted"
+
+    def reject_application(self, request, queryset):
+        queryset.update(status="Rejected")
+    reject_application.short_description = "Mark selected applications as Rejected"
