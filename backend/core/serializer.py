71e1bd7c4524ace29ba8c64f13d54cde15c38de1
surbhi1us <surbhi1us@gmail.com>
2025-09-27 17:10:42 +0530

    Added faculty and project serializer

--- backend/core/serializer.py
+++ backend/core/serializer.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student,Faculty, Project
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'title', 'description']
+
+class FacultySerializer(serializers.ModelSerializer):
+    projects = ProjectSerializer(many=True, read_only=True)  # related_name for reverse FK
+
+    class Meta:
+        model = Faculty
+        fields = ['id', 'name', 'department', 'projects']
